2617c52964bb91a08f62590c75bff7a58a3c8b9d
EJBivona <EJBivona@gmail.com>
2015-09-02 16:50:17 -0400

    Removed permission calculation as the is_admin flag covers it.

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -13,45 +13,14 @@ begin
 # Create our initial user, if we need to bootstrap access to the
 # system: since we override the basic username/password authentication
 # mechanism, there isn't any way to log in as the admin user when this
-# is installed.
-  if ((initUserInfo = AppConfig[:omniauthCas][:initialUser]))
-
-#   Generate the set of permissions we want our initial user to have.
-    permissions     = []
-    permissionCodes = [ 'system_config', 'administer_system', 'manage_users', 'become_user' ]
-    Permission.each do |permission|
-      if ((idx = permissionCodes.find_index { |code|
-             code == permission.permission_code
-           }) != nil)
-        permissions.push(permission)
-        permissionCodes.delete_at(idx)
-        logger.info("Found permission #{permission.permission_code} (#{permissionCodes.length} remain).")####
-      end
-    end
-
+# plugin is installed.
+  if ((initUserInfo = AppConfig[:omniauthCas][:initialUser])
+        && !(initialUser = User.find(:username => initUserInfo[:username])))
 #   Create our initial user.
-    if (!(initialUser = User.find(:username => initUserInfo[:username])))
-      initialUser = JSONModel(:user).from_hash('username'   => initUserInfo[:username],
-                                               'name'       => initUserInfo[:name],
-                                               'is_admin'   => true)
-    end
-    ####logger.info("omniauthCas/backend: initialUser='#{initialUser}'")####
-
-#   Configure our initial user.
-    begin
-      initialUser.save(:password    => SecureRandom.hex,
-                       :permissions => permissions)
-    rescue ValidationException => error
-      logger.error("omniauthCas/backend: initialUser.save: #{error}")####
-      initialUser.update(:permissions => permissions)
-      initialUser.refetch    
-    end
-
-    logger.info("omniauthCas/backend: Created/updated initial user account for username '#{initialUser.username}' with permissions:")####
-    initialUser.permissions.each do |permission|####
-      logger.info("#{permission}")####
-    end####
-
+    initialUser = JSONModel(:user).from_hash('username'   => initUserInfo[:username],
+                                             'name'       => initUserInfo[:name],
+                                             'is_admin'   => true)
+    logger.info("omniauthCas/backend: initialUser='#{initialUser}'")####
   end
 
 end
