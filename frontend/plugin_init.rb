3d8ed17ea23bf683585527c7edcc03f7e15cab6b
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2020-11-18 09:55:32 -0500

    merge github updates

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -5,9 +5,33 @@ require 'omniauth-cas'
 
 OmniAuth.config.logger = ASpaceLogger.new($stderr)
 
+ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
+
+unless AppConfig[:omniauthCas][:full_host].blank?
+  OmniAuth.config.full_host = AppConfig[:omniauthCas][:full_host]
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas,
            AppConfig[:omniauthCas][:provider]
 end
 
-ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
+Rails.application.config.after_initialize do
+  # remove the option to start a login from the front end unless it goes through omniauth controller
+  class SessionController < ApplicationController
+  
+    def login
+      backend_session = nil
+  
+      if backend_session
+        User.establish_session(self, backend_session, params[:username])
+      end
+  
+      load_repository_list
+  
+      render :json => {:session => backend_session, :csrf_token => form_authenticity_token}
+    end
+    
+  end
+
+end
